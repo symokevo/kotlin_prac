9e88491af06cc72cce34f53aeb929816652371b9
simon <simon@localhost.localdomain>
2021-04-23 20:23:21 +0300

    initial commit

---
+++ BirthdayCake.kts
@@ -0,0 +1,37 @@
+fun main() {
+    val age = 24
+    val layers = 5
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
+
+}
+
+fun printCakeTop(age: Int) {
+    repeat(age + 2){
+        print("=")
+    }
+    println()
+}
+
+fun printCakeCandles(age: Int) {
+    repeat(age) {
+        print(",")
+    }
+    println()
+    print("")
+    repeat(age) {
+        print("|")
+    }
+    println()
+}
+
+fun printCakeBottom(age: Int, layers: Int) {
+    print(" ")
+    repeat(layers) {
+        repeat(age + 2){
+            print("@")
+        }
+        println()
+    }
+}
